4fa9259a5e8e4fc69dc801a97507ed68ad47102d
dagasolo138-lgtm <dagasolo138@gmail.com>
2026-07-14 12:56:15 +0800

    feat(agent-economy): export daily simulation modules

--- src/engine/agentEconomy/index.js
+++ src/engine/agentEconomy/index.js
@@ -33,3 +33,41 @@ export {
   normalizeNeeds,
   updateNeed,
 } from "./needsSystem.js";
+
+export {
+  DEFAULT_AGENT_ECONOMY_SEED,
+  createSeededRng,
+  normalizeSeed,
+  stochasticRound,
+} from "./seededRng.js";
+
+export {
+  DAILY_PRODUCTION_RECIPES,
+  getHouseholdProductivity,
+  produceHousehold,
+} from "./productionSystem.js";
+
+export {
+  consumeHousehold,
+  updateHouseholdNeeds,
+} from "./consumptionSystem.js";
+
+export {
+  generateHouseholdOrderIntents,
+  previewOrderMatches,
+} from "./orderIntentSystem.js";
+
+export {
+  DAILY_INCOME_BY_OCCUPATION,
+  applyHouseholdTaxAndWelfare,
+  payHouseholdIncome,
+  updateHouseholdWellbeing,
+} from "./welfareSystem.js";
+
+export {
+  AGENT_DAYS_PER_QUARTER,
+  DAILY_PIPELINE,
+  getAgentEconomyTotals,
+  simulateAgentDay,
+  simulateAgentQuarter,
+} from "./dailySimulation.js";
